40ac7de3f04efdfe2e6cdf98a4d76be75bb607d1
Nadun Kawishika <hewagenkm@gmail.com>
2024-06-24 15:25:19 +0530

    Initial commit

---
+++ UserService/src/test/java/com/hewagenkm/userservice/UserServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hewagenkm.userservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
